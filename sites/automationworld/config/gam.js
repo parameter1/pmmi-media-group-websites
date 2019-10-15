a8e3208c188dfc6cf8a60247d99253f6436ce81d
Josh Worden <solocommand@gmail.com>
2019-10-15 18:33:54 -0500

    cleanup ad units

--- sites/automationworld/config/gam.js
+++ sites/automationworld/config/gam.js
@@ -16,20 +16,10 @@ config
   .setAliasAdUnits('default', [
     { name: 'leaderboard', templateName: 'leaderboard', path: 'aw_leaderboard' },
     { name: 'article-top-below-head', templateName: 'leaderboard', path: 'aw-article-top-below-head' },
-    { name: 'article-01-right', path: 'aw-article-01-right' },
-    { name: 'article-leaders-vote-btn', path: 'aw-article-leaders-vote-btn' },
-    { name: 'article-02-right', path: 'aw-article-02-right' },
-    { name: 'article-02-right', path: 'aw-article-03-right' },
-    { name: 'article-04-left', path: 'aw-article-04-left' },
     { name: 'home-top-below-head', templateName: 'leaderboard', path: 'aw-home-top-below-head' },
-    { name: 'home-01-right', path: 'aw-home-01-right' },
-    { name: 'home-leaders-vote-btn', path: 'aw-home-leaders-vote-btn' },
-    { name: 'home-02-right', path: 'aw-home-02-right' },
-    { name: 'home-03-right', path: 'aw-home-03-right' },
-    { name: 'home-04-left', path: 'aw-home-04-left' },
     { name: 'imu1', size: [300, 250], path: 'aw_imu_1' },
     { name: 'imu2', size: [300, 250], path: 'aw_imu_2' },
-    { name: 'skyscraper', size: [300, 600], path: 'aw_skyscraper' },
+    { name: 'skyscraper', size: [300, 600], path: 'aw_fdtskyscraper' },
   ]);
 
 module.exports = config;
