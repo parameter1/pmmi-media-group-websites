cbb754c06772c04f38730aeb78c5584ca9d43c6b
Josh Worden <solocommand@gmail.com>
2019-10-15 20:40:29 -0500

    Bottom, load more

--- sites/automationworld/config/native-x.js
+++ sites/automationworld/config/native-x.js
@@ -5,6 +5,7 @@ const config = new NativeXConfiguration('https://pmmi.native-x.io');
 config
   .setAliasPlacements('default', [
     { name: 'syndicated-content', id: '5da64b96d5766a00013c3947' },
+    { name: 'load-more', id: '5da66dbed5766a00013c3dd8' },
   ]);
 
 module.exports = config;
