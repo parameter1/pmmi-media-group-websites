22e4c91cf0ca6f71cd718d26f512eec5275d498c
Brandon Krigbaum <brandonbk@gmail.com>
2019-10-21 11:03:59 -0500

    Updated native-x configs

--- sites/profoodworld/config/native-x.js
+++ sites/profoodworld/config/native-x.js
@@ -2,11 +2,9 @@ const NativeXConfiguration = require('@base-cms/marko-web-native-x/config');
 
 const config = new NativeXConfiguration('https://pmmi.native-x.io');
 
-// config
-//   .setAliasPlacements('default', [
-//     { name: 'card', id: '5cdb230339937800010ea6ca' },
-//     { name: 'list2', id: '5cdb2308f41dfb0001fce77f' },
-//     { name: 'list1', id: '5cdb230ef41dfb0001fce866' },
-//   ]);
+config
+  .setAliasPlacements('default', [
+    { name: 'load-more', id: '5da778bd65ebb90001f3ceab' },
+  ]);
 
 module.exports = config;
