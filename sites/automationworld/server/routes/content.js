60f2869ec1ced4345311666f5f7ce3e683f69da0
Brandon Krigbaum <brandonbk@gmail.com>
2019-10-24 07:52:50 -0500

    Added custom webinar layout to all pmmi sites

--- sites/automationworld/server/routes/content.js
+++ sites/automationworld/server/routes/content.js
@@ -1,8 +1,13 @@
 const { withContent } = require('@base-cms/marko-web/middleware');
 const content = require('../templates/content');
+const webinar = require('../templates/content/webinar');
 const queryFragment = require('../graphql/fragments/content-page');
 
 module.exports = (app) => {
+  app.get('/*?webinar/:id(\\d{8})*', withContent({
+    template: webinar,
+    queryFragment,
+  }));
   app.get('/*?:id(\\d{8})*', withContent({
     template: content,
     queryFragment,
