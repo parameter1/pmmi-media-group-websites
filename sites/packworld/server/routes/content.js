6475e79de7c24f3cfa91a29ff0bee77011b3fc98
Brian Miller <bmiller@southcomm.com>
2019-10-29 17:58:09 -0500

    move content-page fragment to common

--- sites/packworld/server/routes/content.js
+++ sites/packworld/server/routes/content.js
@@ -2,7 +2,7 @@ const { withContent } = require('@base-cms/marko-web/middleware');
 const content = require('../templates/content');
 const company = require('../templates/content/company');
 const webinar = require('../templates/content/webinar');
-const queryFragment = require('../graphql/fragments/content-page');
+const queryFragment = require('../../../../packages/common/graphql/fragments/content-page');
 const companyQueryFragment = require('../../../../packages/common/graphql/fragments/content-company');
 
 module.exports = (app) => {
