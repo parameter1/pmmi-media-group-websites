fc451c1de84a905f4a6d2a98e99acab98deedd57
Brian Miller <bmiller@southcomm.com>
2019-10-30 11:46:13 -0500

    static under construction page

--- sites/profoodworld/server/routes/website-section.js
+++ sites/profoodworld/server/routes/website-section.js
@@ -1,12 +1,12 @@
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const section = require('../templates/website-section');
 const contactUs = require('../templates/website-section/contact-us');
-const global250 = require('../templates/website-section/global250');
+const global250 = require('../templates/website-section/global-250');
 const global50 = require('../templates/website-section/global-50');
 const queryFragment = require('../graphql/fragments/website-section-page');
 
 module.exports = (app) => {
-  app.get('/global250', (_, res) => { res.marko(global250); });
+  app.get('/global-250', (_, res) => { res.marko(global250); });
   app.get('/global-50', (_, res) => { res.marko(global50); });
 
   app.get('/:alias(contact-us)', withWebsiteSection({
