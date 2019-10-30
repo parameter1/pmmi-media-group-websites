ba0e4387659b758438b5ebec7c3a561a7387dade
Brian Miller <bmiller@southcomm.com>
2019-10-30 14:12:25 -0500

    add pack world search

--- sites/packworld/config/gcse.js
+++ sites/packworld/config/gcse.js
@@ -1,5 +1,5 @@
 const GCSEConfiguration = require('@base-cms/marko-web-gcse/config');
 
-const config = new GCSEConfiguration('003355913687346718228:znjzr8aiziw');
+const config = new GCSEConfiguration('003355913687346718228:trgtl90veyh');
 
 module.exports = config;
