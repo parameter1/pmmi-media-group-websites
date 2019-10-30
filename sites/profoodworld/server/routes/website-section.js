41543273b4c97d6a800bad787fead52082223a1a
Brian Miller <bmiller@southcomm.com>
2019-10-30 08:34:55 -0500

    add temp global 250 & 50 temp pages

--- sites/profoodworld/server/routes/website-section.js
+++ sites/profoodworld/server/routes/website-section.js
@@ -1,9 +1,14 @@
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const section = require('../templates/website-section');
 const contactUs = require('../templates/website-section/contact-us');
+const global250 = require('../templates/website-section/global250');
+const global50 = require('../templates/website-section/global-50');
 const queryFragment = require('../graphql/fragments/website-section-page');
 
 module.exports = (app) => {
+  app.get('/global250', (_, res) => { res.marko(global250); });
+  app.get('/global-50', (_, res) => { res.marko(global50); });
+
   app.get('/:alias(contact-us)', withWebsiteSection({
     template: contactUs,
     queryFragment,
