9a9be75325400b4aaeb97da5ceb3a3d97e5a9ad0
Josh Worden <solocommand@gmail.com>
2019-11-04 15:56:25 -0600

    Add PFW

--- sites/profoodworld/server/routes/website-section.js
+++ sites/profoodworld/server/routes/website-section.js
@@ -1,14 +1,19 @@
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
+const leadersFragment = require('@base-cms-websites/package-common/graphql/fragments/leaders-section');
 const section = require('../templates/website-section');
 const contactUs = require('../templates/website-section/contact-us');
 const global250 = require('../templates/website-section/global-250');
 const global50 = require('../templates/website-section/global-50');
+const leaders = require('../templates/website-section/leaders');
 const queryFragment = require('../graphql/fragments/website-section-page');
 
 module.exports = (app) => {
   app.get('/global-250', (_, res) => { res.marko(global250); });
   app.get('/global-50', (_, res) => { res.marko(global50); });
-
+  app.get('/:alias(leaders)', withWebsiteSection({
+    template: leaders,
+    queryFragment: leadersFragment,
+  }));
   app.get('/:alias(contact-us)', withWebsiteSection({
     template: contactUs,
     queryFragment,
