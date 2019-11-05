aaa10800d347d27ac6826e9ef2eeac711498de3e
Brian Miller <bmiller@southcomm.com>
2019-11-05 08:35:39 -0600

    fix the image references on PMMI sites

--- sites/healthcarepackaging/config/site.js
+++ sites/healthcarepackaging/config/site.js
@@ -5,15 +5,15 @@ module.exports = {
   company: 'PMMI Media Group',
   logos: {
     navbar: {
-      src: 'https://base.imgix.net/files/base/pmmi/hcp/hcp_logo.png?h=45',
+      src: 'https://img.healthcarepackaging.com/files/base/pmmi/hcp/hcp_logo.png?h=45',
       srcset: [
-        'https://base.imgix.net/files/base/pmmi/hcp/hcp_logo.png?h=90 2x',
+        'https://img.healthcarepackaging.com/files/base/pmmi/hcp/hcp_logo.png?h=90 2x',
       ],
     },
     footer: {
-      src: 'https://base.imgix.net/files/base/pmmi/hcp/hcp_logo.png?h=60',
+      src: 'https://img.healthcarepackaging.com/files/base/pmmi/hcp/hcp_logo.png?h=60',
       srcset: [
-        'https://base.imgix.net/files/base/pmmi/hcp/hcp_logo.png?h=120 2x',
+        'https://img.healthcarepackaging.com/files/base/pmmi/hcp/hcp_logo.png?h=120 2x',
       ],
     },
   },
--- sites/oemmagazine/config/site.js
+++ sites/oemmagazine/config/site.js
@@ -5,15 +5,15 @@ module.exports = {
   company: 'PMMI Media Group',
   logos: {
     navbar: {
-      src: 'https://base.imgix.net/files/base/pmmi/oem/oem_logo.png?h=45',
+      src: 'https://img.oemmagazine.com/files/base/pmmi/oem/oem_logo.png?h=45',
       srcset: [
-        'https://base.imgix.net/files/base/pmmi/oem/oem_logo.png?h=90 2x',
+        'https://img.oemmagazine.com/files/base/pmmi/oem/oem_logo.png?h=90 2x',
       ],
     },
     footer: {
-      src: 'https://base.imgix.net/files/base/pmmi/oem/oem_logo.png?h=60',
+      src: 'https://img.oemmagazine.com/files/base/pmmi/oem/oem_logo.png?h=60',
       srcset: [
-        'https://base.imgix.net/files/base/pmmi/oem/oem_logo.png?h=120 2x',
+        'https://img.oemmagazine.com/files/base/pmmi/oem/oem_logo.png?h=120 2x',
       ],
     },
   },
