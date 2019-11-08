80949ba23b5a9df98f6b867a9c78cacef749486e
Jacob Bare <jacob@limit0.io>
2019-11-08 08:28:55 -0600

    Update AW leaders image alt

--- sites/automationworld/config/leaders.js
+++ sites/automationworld/config/leaders.js
@@ -2,6 +2,6 @@ module.exports = {
   alias: 'leaders',
   header: {
     imgSrc: 'https://img.automationworld.com/files/base/pmmi/all/leaders/aw.png',
-    imgAlt: 'Leaders in Automation 2019',
+    imgAlt: 'Leadership in Automation 2019',
   },
 };
