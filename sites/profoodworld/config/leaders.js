c0212a125107213053551d83526ffe4a8813fe1c
Jacob Bare <jacob@limit0.io>
2019-11-08 08:43:47 -0600

    Configure PFW leaders and use home component

---
+++ sites/profoodworld/config/leaders.js
@@ -0,0 +1,7 @@
+module.exports = {
+  alias: 'leaders',
+  header: {
+    imgSrc: 'https://img.profoodworld.com/files/base/pmmi/all/leaders/pfw.png',
+    imgAlt: 'Leaders in Processing 2019',
+  },
+};
