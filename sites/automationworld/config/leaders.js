6a9ed20aa11ea3ce9c43a81afeaf1c6f7921d8bb
Jacob Bare <jacob@limit0.io>
2019-11-08 08:22:05 -0600

    Configure AW leaders and use home component

---
+++ sites/automationworld/config/leaders.js
@@ -0,0 +1,7 @@
+module.exports = {
+  alias: 'leaders',
+  header: {
+    imgSrc: 'https://img.automationworld.com/files/base/pmmi/all/leaders/aw.png',
+    imgAlt: 'Leaders in Automation 2019',
+  },
+};
