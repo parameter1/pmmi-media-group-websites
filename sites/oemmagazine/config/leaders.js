cc60d62f32a218bb14a0f74abb06c5fdb5d8d4ed
Jacob Bare <jacob@limit0.io>
2019-11-08 08:33:18 -0600

    Configure OEM leaders and use home component

---
+++ sites/oemmagazine/config/leaders.js
@@ -0,0 +1,7 @@
+module.exports = {
+  alias: 'leaders',
+  header: {
+    imgSrc: 'https://img.oemmagazine.org/files/base/pmmi/all/leaders/oem.png',
+    imgAlt: 'OEM Tech Trend Setters 2019',
+  },
+};
