3b0a3b3a7202010941db0941ddb9471bc4e7236b
Jacob Bare <jacob@limit0.io>
2019-11-08 07:41:44 -0600

    Import new leaders browser components

--- packages/common/browser/index.js
+++ packages/common/browser/index.js
@@ -1,13 +1,12 @@
-import LeadersItem from '../leaders/browser/item.vue';
-import LeadersItemStatic from '../leaders/browser/item-static.vue';
+import Leaders from '@base-cms/marko-web-leaders/browser';
 
 const ImageSlider = () => import(/* webpackChunkName: "common-image-slider" */ './image-slider.vue');
 const ContactUsForm = () => import(/* webpackChunkName: "common-contact-us" */ './contact-us-form.vue');
 
 export default (Browser) => {
+  Leaders(Browser);
+
   // @todo this should be removed once contact us is moved to core.
   Browser.register('CommonContactUsForm', ContactUsForm);
   Browser.register('CommonImageSlider', ImageSlider);
-  Browser.register('CommonLeadersItem', LeadersItem);
-  Browser.register('CommonLeadersItemStatic', LeadersItemStatic);
 };
