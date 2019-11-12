61b4891706946cf04e75f7930e2d90e1d437679b
Josh Worden <solocommand@gmail.com>
2019-11-12 15:50:34 -0600

    Rename component

--- packages/common/browser/index.js
+++ packages/common/browser/index.js
@@ -2,11 +2,11 @@ import Leaders from '@base-cms/marko-web-leaders/browser';
 
 const ImageSlider = () => import(/* webpackChunkName: "common-image-slider" */ './image-slider.vue');
 const ContactUsForm = () => import(/* webpackChunkName: "common-contact-us" */ './contact-us-form.vue');
-const TrackLeaders = () => import(/* webpackChunkName: "common-gtm-track-leaders" */ './track-leaders.vue');
+const CommonGTMTrackLeaders = () => import(/* webpackChunkName: "common-gtm-track-leaders" */ './gtm-track-leaders.vue');
 
 export default (Browser) => {
   const { EventBus } = Browser;
-  Browser.register('TrackLeaders', TrackLeaders, { provide: { EventBus } });
+  Browser.register('CommonGTMTrackLeaders', CommonGTMTrackLeaders, { provide: { EventBus } });
   Leaders(Browser);
 
   // @todo this should be removed once contact us is moved to core.
