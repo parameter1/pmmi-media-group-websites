65ba2db5e7a6ad291a80eeb830b25ef1323620d2
Josh Worden <solocommand@gmail.com>
2019-11-12 14:26:20 -0600

    Provide EventBus to leaders program
    Requires base-cms/base-cms#236

--- packages/common/browser/index.js
+++ packages/common/browser/index.js
@@ -1,13 +1,12 @@
-import Leaders from '@base-cms/marko-web-leaders/browser';
-
+const LeadersProgram = () => import(/* webpackChunkName: "leaders-program" */ '@base-cms/leaders-program');
 const ImageSlider = () => import(/* webpackChunkName: "common-image-slider" */ './image-slider.vue');
 const ContactUsForm = () => import(/* webpackChunkName: "common-contact-us" */ './contact-us-form.vue');
 const TrackLeaders = () => import(/* webpackChunkName: "common-gtm-track-leaders" */ './track-leaders.vue');
 
 export default (Browser) => {
-  Leaders(Browser);
   const { EventBus } = Browser;
   Browser.register('TrackLeaders', TrackLeaders, { provide: { EventBus } });
+  Browser.register('LeadersProgram', LeadersProgram, { withApollo: true, provide: { EventBus } });
 
   // @todo this should be removed once contact us is moved to core.
   Browser.register('CommonContactUsForm', ContactUsForm);
