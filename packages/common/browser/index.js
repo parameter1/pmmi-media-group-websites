2a148cc0f24721886c3c294519fbe17f172705e3
Josh Worden <solocommand@gmail.com>
2019-11-12 15:16:05 -0600

    Revert custom leaders import

--- packages/common/browser/index.js
+++ packages/common/browser/index.js
@@ -1,4 +1,5 @@
-const LeadersProgram = () => import(/* webpackChunkName: "leaders-program" */ '@base-cms/leaders-program');
+import Leaders from '@base-cms/marko-web-leaders/browser';
+
 const ImageSlider = () => import(/* webpackChunkName: "common-image-slider" */ './image-slider.vue');
 const ContactUsForm = () => import(/* webpackChunkName: "common-contact-us" */ './contact-us-form.vue');
 const TrackLeaders = () => import(/* webpackChunkName: "common-gtm-track-leaders" */ './track-leaders.vue');
@@ -6,7 +7,7 @@ const TrackLeaders = () => import(/* webpackChunkName: "common-gtm-track-leaders
 export default (Browser) => {
   const { EventBus } = Browser;
   Browser.register('TrackLeaders', TrackLeaders, { provide: { EventBus } });
-  Browser.register('LeadersProgram', LeadersProgram, { withApollo: true, provide: { EventBus } });
+  Leaders(Browser);
 
   // @todo this should be removed once contact us is moved to core.
   Browser.register('CommonContactUsForm', ContactUsForm);
