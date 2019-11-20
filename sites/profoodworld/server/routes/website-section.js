2971501817cc504b678da5a2a8335d48eae7ad09
Brian Miller <bmiller@southcomm.com>
2019-11-20 09:02:09 -0600

    add routing for archived global25/50 pages
    dynamic data comes from gtm

--- sites/profoodworld/server/routes/website-section.js
+++ sites/profoodworld/server/routes/website-section.js
@@ -5,11 +5,23 @@ const contactUs = require('../templates/website-section/contact-us');
 const global250 = require('../templates/website-section/global-250');
 const global50 = require('../templates/website-section/global-50');
 const leaders = require('../templates/website-section/leaders');
+const mia = require('../templates/website-section/awards/mia');
 const queryFragment = require('../graphql/fragments/website-section-page');
 
 module.exports = (app) => {
   app.get('/global-250', (_, res) => { res.marko(global250); });
+  app.get('/global-250/2017', (_, res) => { res.marko(global250); });
+  app.get('/global-250/2016', (_, res) => { res.marko(global250); });
+
   app.get('/global-50', (_, res) => { res.marko(global50); });
+  app.get('/global-50/2017', (_, res) => { res.marko(global50); });
+  app.get('/global-50/2016', (_, res) => { res.marko(global50); });
+
+  app.get('/:alias(awards/mia)', withWebsiteSection({
+    template: mia,
+    queryFragment,
+  }));
+
   app.get('/:alias(leaders)', withWebsiteSection({
     template: leaders,
     queryFragment: leadersFragment,
