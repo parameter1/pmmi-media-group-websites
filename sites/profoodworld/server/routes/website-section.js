7023bd502ecf4cd1c1cc69e7298befe6dd4f286b
Brian Miller <bmiller@southcomm.com>
2019-11-20 09:34:15 -0600

    remove invalid route

--- sites/profoodworld/server/routes/website-section.js
+++ sites/profoodworld/server/routes/website-section.js
@@ -5,7 +5,6 @@ const contactUs = require('../templates/website-section/contact-us');
 const global250 = require('../templates/website-section/global-250');
 const global50 = require('../templates/website-section/global-50');
 const leaders = require('../templates/website-section/leaders');
-const mia = require('../templates/website-section/awards/mia');
 const queryFragment = require('../graphql/fragments/website-section-page');
 
 module.exports = (app) => {
@@ -16,11 +15,6 @@ module.exports = (app) => {
   app.get('/global-50', (_, res) => { res.marko(global50); });
   app.get('/global-50/2017', (_, res) => { res.marko(global50); });
   app.get('/global-50/2016', (_, res) => { res.marko(global50); });
-
-  app.get('/:alias(awards/mia)', withWebsiteSection({
-    template: mia,
-    queryFragment,
-  }));
 
   app.get('/:alias(leaders)', withWebsiteSection({
     template: leaders,
