30ccb35a03134e3d75c2bf9a4d52f2dc1b26b9f5
Josh Worden <solocommand@gmail.com>
2020-01-10 09:42:33 -0600

    update 2020 logos

--- sites/automationworld.com/config/leaders.js
+++ sites/automationworld.com/config/leaders.js
@@ -2,6 +2,6 @@ module.exports = {
   title: 'Leadership in Automation',
   alias: 'leaders-2020',
   header: {
-    imgSrc: 'https://img.automationworld.com/files/base/pmmi/all/leaders/aw.png',
+    imgSrc: 'https://img.automationworld.com/files/base/pmmi/all/leaders/aw-2020.png',
   },
 };
