421f6403ec75e05acb654e6db58089bc1564b9e1
Josh Worden <solocommand@gmail.com>
2020-01-10 10:18:54 -0600

    max height

--- sites/packworld.com/config/leaders.js
+++ sites/packworld.com/config/leaders.js
@@ -2,6 +2,6 @@ module.exports = {
   title: 'Leaders in Packaging',
   alias: 'leaders-2020',
   header: {
-    imgSrc: 'https://img.packworld.com/files/base/pmmi/all/leaders/pw-2020.png',
+    imgSrc: 'https://img.packworld.com/files/base/pmmi/all/leaders/pw-2020.png?h=90',
   },
 };
