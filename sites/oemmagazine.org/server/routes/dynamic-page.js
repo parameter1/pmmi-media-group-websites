2c65e867888d27f673c555e238ec79602300c0ef
Josh Worden <solocommand@gmail.com>
2020-04-27 09:33:05 -0500

    Use rolled up template for dynamic pages

--- sites/oemmagazine.org/server/routes/dynamic-page.js
+++ sites/oemmagazine.org/server/routes/dynamic-page.js
@@ -1,6 +1,6 @@
 const { withDynamicPage } = require('@base-cms/marko-web/middleware');
-const page = require('../templates/dynamic-page');
-const queryFragment = require('../graphql/fragments/dynamic-page');
+const page = require('@pmmi-media-group/package-shared/templates/dynamic-page');
+const queryFragment = require('@pmmi-media-group/package-shared/graphql/fragments/dynamic-page');
 
 module.exports = (app) => {
   app.get('/page/:alias(*)', withDynamicPage({
