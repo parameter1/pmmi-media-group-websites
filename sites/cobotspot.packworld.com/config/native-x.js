32f95cfb6ef905e7b4079ad9cf3e98c7380777eb
Brandon Krigbaum <brandonbk@gmail.com>
2020-05-18 10:36:02 -0500

    Update native-x config

--- sites/cobotspot.packworld.com/config/native-x.js
+++ sites/cobotspot.packworld.com/config/native-x.js
@@ -4,7 +4,7 @@ const config = new NativeXConfiguration('https://pmmi.native-x.io');
 
 config
   .setAliasPlacements('default', [
-    { name: 'load-more', id: '5da778d7d5766a00013c459d' },
+    { name: 'load-more', id: '5ec2ab49029a4e0001599842' },
   ]);
 
 module.exports = config;
