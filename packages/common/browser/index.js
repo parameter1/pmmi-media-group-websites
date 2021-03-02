3966db19d6104c2caaee6d9b5d4b41838c320e33
Jacob Bare <jacob@parameter1.com>
2021-03-02 12:22:34 -0600

    Load leaders from shared using base cms package

--- packages/common/browser/index.js
+++ packages/common/browser/index.js
@@ -1,22 +1,7 @@
-import Leaders from '@parameter1/base-cms-marko-web-leaders/browser';
-
 const ImageSlider = () => import(/* webpackChunkName: "common-image-slider" */ './image-slider.vue');
 const ContactUsForm = () => import(/* webpackChunkName: "common-contact-us" */ './contact-us-form.vue');
-const CommonGTMTrackLeaders = () => import(/* webpackChunkName: "common-gtm-track-leaders" */ './gtm-track-leaders.vue');
-const CommonLeadersCompanyWebsiteLink = () => import(/* webpackChunkName: "common-leaders-company-website-link" */ './leaders-company-website-link.vue');
-const CommonLeadersCompanySocialLink = () => import(/* webpackChunkName: "common-leaders-company-social-link" */ './leaders-company-social-link.vue');
 
 export default (Browser) => {
-  const { EventBus } = Browser;
-  Browser.register('CommonGTMTrackLeaders', CommonGTMTrackLeaders, { provide: { EventBus } });
-  Browser.register('CommonLeadersCompanyWebsiteLink', CommonLeadersCompanyWebsiteLink, {
-    on: { action: (...args) => EventBus.$emit('leaders-action', ...args) },
-  });
-  Browser.register('CommonLeadersCompanySocialLink', CommonLeadersCompanySocialLink, {
-    on: { action: (...args) => EventBus.$emit('leaders-action', ...args) },
-  });
-  Leaders(Browser);
-
   // @todo this should be removed once contact us is moved to core.
   Browser.register('CommonContactUsForm', ContactUsForm);
   Browser.register('CommonImageSlider', ImageSlider);
--- packages/shared/browser/index.js
+++ packages/shared/browser/index.js
@@ -1,5 +1,7 @@
 import Inquiry from '@parameter1/base-cms-marko-web-inquiry/browser';
+import Leaders from '@parameter1/base-cms-marko-web-leaders/browser';
 
 export default (Browser) => {
   Inquiry(Browser);
+  Leaders(Browser);
 };
