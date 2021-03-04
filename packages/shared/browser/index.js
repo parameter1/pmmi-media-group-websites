73135207bf20c7d394a4d198502c7586965a913b
Jacob Bare <jacob@parameter1.com>
2021-03-04 08:34:54 -0600

    Load P1Events browser components globally

--- packages/shared/browser/index.js
+++ packages/shared/browser/index.js
@@ -1,7 +1,9 @@
 import Inquiry from '@parameter1/base-cms-marko-web-inquiry/browser';
 import Leaders from '@parameter1/base-cms-marko-web-leaders/browser';
+import P1Events from '@parameter1/base-cms-marko-web-p1-events/browser';
 
 export default (Browser) => {
   Inquiry(Browser);
   Leaders(Browser);
+  P1Events(Browser);
 };
--- sites/automationworld.com/browser/index.js
+++ sites/automationworld.com/browser/index.js
@@ -4,11 +4,9 @@ import GTM from '@parameter1/base-cms-marko-web-gtm/browser';
 import GAM from '@parameter1/base-cms-marko-web-gam/browser';
 import GCSE from '@parameter1/base-cms-marko-web-gcse/browser';
 import RevealAd from '@parameter1/base-cms-marko-web-reveal-ad/browser';
-import P1Events from '@parameter1/base-cms-marko-web-p1-events/browser';
 import Common from '@pmmi-media-group/package-common/browser';
 import Shared from '@pmmi-media-group/package-shared/browser';
 
-P1Events(Browser);
 Shared(Browser);
 DefaultTheme(Browser);
 GTM(Browser);
