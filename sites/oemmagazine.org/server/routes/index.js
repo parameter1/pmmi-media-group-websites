119109828192c85b055608b4ff86753a4077d3d2
Brandon Krigbaum <brandonbk@gmail.com>
2021-04-08 16:09:16 -0500

    Add website section routes

--- sites/oemmagazine.org/server/routes/index.js
+++ sites/oemmagazine.org/server/routes/index.js
@@ -1,6 +1,10 @@
 const home = require('./home');
+const websiteSection = require('./website-section');
 
 module.exports = (app) => {
   // Homepage
   home(app);
+
+  // Website Sections
+  websiteSection(app);
 };
