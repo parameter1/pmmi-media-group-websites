65790e3b33301d0e9fbae74b67f73d1a3fcd0d26
Brandon Krigbaum <brandonbk@gmail.com>
2021-04-08 16:00:14 -0500

    Update site website section templates

--- sites/healthcarepackaging.com/server/routes/website-section.js
+++ sites/healthcarepackaging.com/server/routes/website-section.js
@@ -1,18 +1,13 @@
 const { withWebsiteSection } = require('@parameter1/base-cms-marko-web/middleware');
+const queryFragment = require('@pmmi-media-group/package-shared/graphql/fragments/website-section-page');
 const leadersFragment = require('@pmmi-media-group/package-common/graphql/fragments/leaders-section');
 const section = require('../templates/website-section');
-const contactUs = require('../templates/website-section/contact-us');
 const leaders = require('../templates/website-section/leaders');
-const queryFragment = require('../graphql/fragments/website-section-page');
 
 module.exports = (app) => {
   app.get('/:alias(leaders)', withWebsiteSection({
     template: leaders,
     queryFragment: leadersFragment,
-  }));
-  app.get('/:alias(contact-us)', withWebsiteSection({
-    template: contactUs,
-    queryFragment,
   }));
 
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
