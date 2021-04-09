f894bcb92ff46a5dff825bf633e02af788b56ba6
Brandon Krigbaum <brandonbk@gmail.com>
2021-04-09 08:46:12 -0500

    Update GAM config

--- sites/cobotspot.packworld.com/config/gam.js
+++ sites/cobotspot.packworld.com/config/gam.js
@@ -1,11 +1,17 @@
-const GAMConfiguration = require('@parameter1/base-cms-marko-web-gam/config');
+const configureGAM = require('@pmmi-media-group/package-shared/config/gam');
 
-const config = new GAMConfiguration('152023730');
+const config = configureGAM();
 
-config
-  .setAliasAdUnits('default', [
-    { name: 'skyscraper', options: { size: [300, 600] }, path: 'cs_skyscraper' },
-    { name: 'skyscraper-2', options: { size: [300, 600] }, path: 'cs_skyscraper_2' },
-  ]);
+config.lazyLoad = {
+  enabled: false, // set to true to enable lazy loading
+  fetchMarginPercent: 100, // fetch ad when one viewport away
+  renderMarginPercent: 50, // render ad when half viewport away
+  mobileScaling: 2, // double these on mobile
+};
+
+config.setAliasAdUnits('default', [
+  { name: 'skyscraper', options: { size: [300, 600] }, path: 'cs_skyscraper' },
+  { name: 'skyscraper-2', options: { size: [300, 600] }, path: 'cs_skyscraper_2' },
+]);
 
 module.exports = config;
