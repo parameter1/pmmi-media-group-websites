1d0340cdb9f02d9f97f762cd8b5f95171a24d782
Brandon Krigbaum <brandonbk@gmail.com>
2021-04-26 12:29:08 -0500

    Add eventos route and template

--- sites/mundopmmi.com/server/routes/index.js
+++ sites/mundopmmi.com/server/routes/index.js
@@ -1,6 +1,8 @@
 const content = require('@pmmi-media-group/package-shared/routes/content');
 const home = require('./home');
+const publishedContent = require('./published-content');
 const websiteSection = require('./website-section');
+
 
 module.exports = (app) => {
   // Homepage
@@ -8,6 +10,9 @@ module.exports = (app) => {
 
   // Content
   content(app);
+
+  // Published Content
+  publishedContent(app);
 
   // Website Sections
   websiteSection(app);
