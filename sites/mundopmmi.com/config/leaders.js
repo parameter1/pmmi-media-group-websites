44b63215e76fc03a5327fd2fe59763ca3104953a
Brandon Krigbaum <brandonbk@gmail.com>
2021-05-18 21:19:36 -0500

    Update title

--- sites/mundopmmi.com/config/leaders.js
+++ sites/mundopmmi.com/config/leaders.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'Leaders in Packaging',
+  title: 'Proveedores principales',
   alias: process.env.LEADERS_ALIAS || 'leaders/2021',
   header: {
     imgSrc: process.env.LEADERS_LOGO || 'https://img.mundopmmi.com/files/base/pmmi/all/leaders/mundo-leaders-logo.png?h=90',
