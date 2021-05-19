6d3855d9488f56733abc9ba3367ae61c4372b87d
Brandon Krigbaum <brandonbk@gmail.com>
2021-05-18 21:19:15 -0500

    Update mundo leaders alias

--- sites/mundopmmi.com/config/leaders.js
+++ sites/mundopmmi.com/config/leaders.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: 'Leaders in Packaging',
-  alias: process.env.LEADERS_ALIAS || 'leaders-2021',
+  alias: process.env.LEADERS_ALIAS || 'leaders/2021',
   header: {
     imgSrc: process.env.LEADERS_LOGO || 'https://img.packworld.com/files/base/pmmi/all/leaders/pw-2021-2.png?h=90',
   },
