39bb9992a6fa91a202645becec65efc53ca18c5b
Brandon Krigbaum <brandonbk@gmail.com>
2021-05-18 21:20:46 -0500

    Add mundo values

--- sites/mundopmmi.com/config/leaders.js
+++ sites/mundopmmi.com/config/leaders.js
@@ -4,4 +4,7 @@ module.exports = {
   header: {
     imgSrc: process.env.LEADERS_LOGO || 'https://img.mundopmmi.com/files/base/pmmi/all/leaders/mundo-leaders-logo.png?h=90',
   },
+  calloutPrefix: 'BUSQUE ESTOS',
+  calloutValue: 'PROVEEDORES LÍDERES',
+  viewAllText: 'Vea todas las compañías &gt;',
 };
