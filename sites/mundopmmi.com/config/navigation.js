b5c679cda41f87173f679818a6c71579941e8d5b
Brandon Krigbaum <brandonbk@gmail.com>
2021-06-09 18:46:15 -0500

    Move site map to footer, add translation

--- sites/mundopmmi.com/config/navigation.js
+++ sites/mundopmmi.com/config/navigation.js
@@ -24,6 +24,7 @@ module.exports = {
       { href: '/page/privacidad', label: 'Privacidad' },
       { href: '/page/condiciones-de-uso', label: 'Condiciones de uso' },
       { href: '/page/terminos-y-condiciones-de-publicidad', label: 'Términos y condiciones de publicidad' },
+      { href: '/site-map', label: 'Mapa del sitio web' },
     ],
   },
   menu: [
@@ -53,7 +54,6 @@ module.exports = {
         { href: '/page/privacidad', label: 'Privacidad' },
         { href: '/page/condiciones-de-uso', label: 'Condiciones de uso' },
         { href: '/page/terminos-y-condiciones-de-publicidad', label: 'Términos y condiciones de publicidad' },
-        { href: '/site-map', label: 'Site Map' },
       ],
     },
   ],
