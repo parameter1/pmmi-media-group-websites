05c3655bcfa687996216ed54e3701ef0b508a71d
Jacob Bare <jacob@parameter1.com>
2021-11-12 07:38:21 -0600

    Update p1 events identity data on idx state change

--- packages/shared/browser/index.js
+++ packages/shared/browser/index.js
@@ -5,8 +5,23 @@ import P1Events from '@parameter1/base-cms-marko-web-p1-events/browser';
 import SocialSharing from '@parameter1/base-cms-marko-web-social-sharing/browser';
 import OmedaIdentityX from '@parameter1/base-cms-marko-web-omeda-identity-x/browser';
 
+const setP1EventsIdentity = ({ p1events, brandKey, encryptedId }) => {
+  if (!p1events || !brandKey || !encryptedId) return;
+  p1events('setIdentity', `omeda.${brandKey}.customer*${encryptedId}~encrypted`);
+};
 
 export default (Browser) => {
+  const { EventBus } = Browser;
+  EventBus.$on('identity-x-logout', () => {
+    if (window.p1events) window.p1events('setIdentity', null);
+  });
+  EventBus.$on('omeda-identity-x-authenticated', ({ brandKey, encryptedId }) => {
+    setP1EventsIdentity({ p1events: window.p1events, brandKey, encryptedId });
+  });
+  EventBus.$on('omeda-identity-x-rapid-identify-response', ({ brandKey, encryptedId }) => {
+    setP1EventsIdentity({ p1events: window.p1events, brandKey, encryptedId });
+  });
+
   Inquiry(Browser);
   Leaders(Browser);
   IdentityX(Browser);
