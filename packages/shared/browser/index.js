2c06f05862048fad27e0e04668bd21709c247f3c
Jacob Bare <jacob@parameter1.com>
2021-11-13 18:10:43 -0600

    Use consolidated omeda+identityx setup

--- packages/shared/browser/index.js
+++ packages/shared/browser/index.js
@@ -1,6 +1,5 @@
 import Inquiry from '@parameter1/base-cms-marko-web-inquiry/browser';
 import Leaders from '@parameter1/base-cms-marko-web-leaders/browser';
-import IdentityX from '@parameter1/base-cms-marko-web-identity-x/browser';
 import P1Events from '@parameter1/base-cms-marko-web-p1-events/browser';
 import SocialSharing from '@parameter1/base-cms-marko-web-social-sharing/browser';
 import OmedaIdentityX from '@parameter1/base-cms-marko-web-omeda-identity-x/browser';
@@ -24,7 +23,6 @@ export default (Browser) => {
 
   Inquiry(Browser);
   Leaders(Browser);
-  IdentityX(Browser);
   P1Events(Browser);
   SocialSharing(Browser);
   OmedaIdentityX(Browser);
--- packages/shared/config/identity-x.js
+++ packages/shared/config/identity-x.js
@@ -1,38 +1,19 @@
 const IdentityXConfiguration = require('@parameter1/base-cms-marko-web-identity-x/config');
-const addOmedaIntegrationHooks = require('@parameter1/base-cms-marko-web-omeda-identity-x/add-integration-hooks');
-const { get } = require('@parameter1/base-cms-object-path');
 const newrelic = require('newrelic');
-
-const omedaConfig = require('./omeda');
-
-const { log } = console;
 
 module.exports = ({
   appId,
-  apiToken = process.env.IDENTITYX_API_TOKEN,
-  requiredServerFields,
-  requiredClientFields,
-  omedaGraphQLProp = '$omeda',
+  requiredServerFields = ['givenName', 'familyName', 'countryCode'],
+  requiredClientFields = ['regionCode', 'countryCode'],
+  ...rest
 } = {}) => {
   const config = new IdentityXConfiguration({
     appId,
-    apiToken,
+    apiToken: process.env.IDENTITYX_API_TOKEN,
     requiredServerFields,
     requiredClientFields,
-    onHookError: (e) => {
-      if (process.env.NODE_ENV === 'development') {
-        log('ERROR IN IDENTITY-X HOOK', e);
-        if (e.networkError) log('Network Error', get(e, 'networkError.result.errors.0'));
-      }
-      newrelic.noticeError(e);
-    },
-  });
-
-  addOmedaIntegrationHooks({
-    idxConfig: config,
-    brandKey: omedaConfig.brandKey,
-    productId: get(omedaConfig, 'rapidIdentification.productId'),
-    omedaGraphQLProp,
+    onHookError: newrelic.noticeError.bind(newrelic),
+    ...rest,
   });
   return config;
 };
