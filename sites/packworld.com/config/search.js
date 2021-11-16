d127c1cb079fca7e7ffdfbbb58cf2ff7b4c92f8c
Shinsina <shinsina@att.net>
2021-11-16 14:44:13 -0600

    Update PW site and search configs.

---
+++ sites/packworld.com/config/search.js
@@ -0,0 +1,14 @@
+module.exports = {
+  contentTypes: ['Article', 'Blog', 'Company', 'Document', 'MediaGallery', 'News', 'Page', 'Podcast', 'Product', 'Video', 'Webinar'],
+  assignedToWebsiteSectionIds: [
+    64017,
+    33256,
+    33261,
+    33249,
+    74270,
+    74335,
+    71061,
+    70845,
+    63699,
+  ],
+};
