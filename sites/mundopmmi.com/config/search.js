b27476ef48184985914f4a2b38441960917d8d52
Shinsina <shinsina@att.net>
2021-11-16 14:44:01 -0600

    Update Mundo site and search configs.

---
+++ sites/mundopmmi.com/config/search.js
@@ -0,0 +1,10 @@
+module.exports = {
+  contentTypes: ['Article', 'Blog', 'Company', 'Document', 'MediaGallery', 'News', 'Page', 'Podcast', 'Product', 'Video', 'Webinar'],
+  assignedToWebsiteSectionIds: [
+    63704,
+    74628,
+    32560,
+    32561,
+    32562,
+  ],
+};
