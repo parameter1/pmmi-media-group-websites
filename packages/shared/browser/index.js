08c7bab8e27275cb993a5c971393f7f4de4ae582
Shinsina <shinsina@att.net>
2021-11-16 14:43:11 -0600

    Register Search with Browser object.

--- packages/shared/browser/index.js
+++ packages/shared/browser/index.js
@@ -1,6 +1,7 @@
 import Inquiry from '@parameter1/base-cms-marko-web-inquiry/browser';
 import Leaders from '@parameter1/base-cms-marko-web-leaders/browser';
 import P1Events from '@parameter1/base-cms-marko-web-p1-events/browser';
+import Search from '@parameter1/base-cms-marko-web-search/browser';
 import SocialSharing from '@parameter1/base-cms-marko-web-social-sharing/browser';
 import OmedaIdentityX from '@parameter1/base-cms-marko-web-omeda-identity-x/browser';
 
@@ -24,6 +25,7 @@ export default (Browser) => {
   Inquiry(Browser);
   Leaders(Browser);
   P1Events(Browser);
+  Search(Browser);
   SocialSharing(Browser);
   OmedaIdentityX(Browser);
 };
