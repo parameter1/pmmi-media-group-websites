e09a4a725fb6228e73da16c551686f9f03fe1f11
Shinsina <shinsina@att.net>
2021-12-14 11:20:30 -0600

    Update leaders logo on PF, PFW, OEM, AW.

--- sites/packworld.com/config/leaders.js
+++ sites/packworld.com/config/leaders.js
@@ -3,6 +3,6 @@ module.exports = {
   title: 'Leaders in Packaging',
   alias: process.env.LEADERS_ALIAS || 'leaders-2021',
   header: {
-    imgSrc: process.env.LEADERS_LOGO || 'https://img.packworld.com/files/base/pmmi/all/leaders/pw-2021-2.png?h=90',
+    imgSrc: process.env.LEADERS_LOGO || 'https://img.packworld.com/files/base/pmmi/all/leaders/pw-leaders-2022.png?h=90',
   },
 };
