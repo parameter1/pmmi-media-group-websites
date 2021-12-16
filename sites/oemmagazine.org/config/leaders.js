6256d602289370d5626d950cac22f3f6536466d2
Shinsina <46794001+Shinsina@users.noreply.github.com>
2021-12-16 09:28:57 -0600

    Revert "Update leaders logo on PF, PFW, OEM, AW."

--- sites/oemmagazine.org/config/leaders.js
+++ sites/oemmagazine.org/config/leaders.js
@@ -3,6 +3,6 @@ module.exports = {
   title: 'Partner Leaders',
   alias: process.env.LEADERS_ALIAS || 'leaders-2021',
   header: {
-    imgSrc: process.env.LEADERS_LOGO || 'https://img.oemmagazine.org/files/base/pmmi/all/leaders/oem-leaders-2022.png?h=90',
+    imgSrc: process.env.LEADERS_LOGO || 'https://img.oemmagazine.org/files/base/pmmi/all/leaders/oem-2021-2.png?h=90',
   },
 };
--- sites/profoodworld.com/config/leaders.js
+++ sites/profoodworld.com/config/leaders.js
@@ -3,6 +3,6 @@ module.exports = {
   title: 'Leaders in Processing',
   alias: process.env.LEADERS_ALIAS || 'leaders-2021',
   header: {
-    imgSrc: process.env.LEADERS_LOGO || 'https://img.profoodworld.com/files/base/pmmi/all/leaders/pfw-leaders-2022.png?h=90',
+    imgSrc: process.env.LEADERS_LOGO || 'https://img.profoodworld.com/files/base/pmmi/all/leaders/pfw-2021-2.png?h=90',
   },
 };
