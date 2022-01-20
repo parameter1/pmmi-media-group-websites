6c0756c938187e0a042c281c752fc960c5eb3845
Shinsina <shinsina@att.net>
2022-01-20 11:21:50 -0600

    Use site.getAsObject for search configuration.

--- packages/shared/routes/search.js
+++ packages/shared/routes/search.js
@@ -1,14 +1,10 @@
-const { getAsObject } = require('@parameter1/base-cms-object-path');
 const MarkoWebSearchConfig = require('@parameter1/base-cms-marko-web-search/config');
 const middleware = require('@parameter1/base-cms-marko-web-search/middleware');
 const template = require('../templates/search');
 
 module.exports = (app) => {
   const { site } = app.locals;
-  const {
-    contentTypes,
-    assignedToWebsiteSectionIds,
-  } = getAsObject(getAsObject(site, 'config'), 'search');
+  const { contentTypes, assignedToWebsiteSectionIds } = site.getAsObject('search');
   const config = new MarkoWebSearchConfig({
     resultsPerPage: { default: 18 },
     contentTypes,
