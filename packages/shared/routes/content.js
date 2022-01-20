8b4a53a1c182c7ef9654a263b863c4f0b1f5a4aa
Shinsina <shinsina@att.net>
2022-01-20 10:14:45 -0600

    Rename imported fragment factories accordingly.

--- packages/shared/routes/content.js
+++ packages/shared/routes/content.js
@@ -1,6 +1,6 @@
 const { withContent } = require('@parameter1/base-cms-marko-web/middleware');
-const companyQueryFragment = require('@pmmi-media-group/package-common/graphql/fragments/content-company');
-const queryFragment = require('../graphql/fragments/content-page');
+const companyQueryFragmentFn = require('@pmmi-media-group/package-common/graphql/fragment-factories/content-company');
+const contentQueryFragmentFn = require('../graphql/fragment-factories/content-page');
 const contactQueryFragment = require('../graphql/fragments/content-contact');
 const company = require('../templates/content/company');
 const content = require('../templates/content');
@@ -13,7 +13,7 @@ module.exports = (app) => {
   const { site } = app.locals;
   app.get('/*?company/:id(\\d{8})*', withContent({
     template: company,
-    queryFragment: companyQueryFragment(site.get('leaders.alias')),
+    queryFragment: companyQueryFragmentFn(site.get('leaders.alias')),
   }));
   app.get('/*?contact/:id(\\d{8})*', withContent({
     template: contact,
@@ -21,19 +21,18 @@ module.exports = (app) => {
   }));
   app.get('/*?webinar/:id(\\d{8})*', withContent({
     template: webinar,
-    queryFragment: queryFragment(site.get('leaders.alias')),
+    queryFragment: contentQueryFragmentFn(site.get('leaders.alias')),
   }));
   app.get('/*?podcast/:id(\\d{8})*', withContent({
     template: podcast,
-    queryFragment: queryFragment(site.get('leaders.alias')),
+    queryFragment: contentQueryFragmentFn(site.get('leaders.alias')),
   }));
   app.get('/*?document/:id(\\d{8})*', withContent({
     template: document,
-    queryFragment: queryFragment(site.get('leaders.alias')),
+    queryFragment: contentQueryFragmentFn(site.get('leaders.alias')),
   }));
   app.get('/*?:id(\\d{8})*', withContent({
     template: content,
-    queryFragment: queryFragment(site.get('leaders.alias')),
+    queryFragment: contentQueryFragmentFn(site.get('leaders.alias')),
   }));
-  app.get('/hooplah', () => console.log(site.get('leaders.alias')));
 };
--- packages/shared/routes/print-content.js
+++ packages/shared/routes/print-content.js
@@ -1,12 +1,12 @@
 const { withContent } = require('@parameter1/base-cms-marko-web/middleware');
 const print = require('../templates/content/print');
-const queryFragment = require('../graphql/fragments/content-page');
+const contentQueryFragmentFn = require('../graphql/fragment-factories/content-page');
 
 module.exports = (app) => {
   const { site } = app.locals;
   app.get('/print/content/:id(\\d{8})', withContent({
     template: print,
-    queryFragment: queryFragment(site.get('leaders.alias')),
+    queryFragment: contentQueryFragmentFn(site.get('leaders.alias')),
     redirectOnPathMismatch: false,
   }));
 };
