ad803a59c961878351b532a7f68be681dce6ae4b
Brian Miller <brian@parameter1.com>
2022-01-31 15:37:38 -0600

    udate navigation to match new global structure

--- sites/mundopmmi-global/config/navigation.js
+++ sites/mundopmmi-global/config/navigation.js
@@ -1,22 +1,73 @@
+const topics = {
+  primary: [
+    { href: '/automatizacion', label: 'Automatización' },
+    { href: '/empaque', label: 'Empaque' },
+    { href: '/procesamiento', label: 'Procesamiento' },
+    { href: '/eventos', label: 'Eventos' },
+    { href: '/downloads', label: 'Recursos Digitales' },
+  ],
+  expanded: [],
+  secondary: [
+    { href: '/reader-rigs', label: 'Reader Rigs' },
+    { href: '/overdrive-radio', label: 'Overdrive Radio' },
+    { href: '/videos', label: 'Videos' },
+    { href: '/white-papers', label: 'Whitepapers' },
+  ],
+};
+
+const utilities = [
+  { href: 'https://www.pmmimediagroup.com/pw/mundo-pmmi', label: 'Anuncie', target: '_blank' },
+  { href: '/page/contacto', label: 'Contacto' },
+  { href: '/page/privacidad', label: 'Privacidad' },
+  { href: '/page/condiciones-de-uso', label: 'Condiciones de uso' },
+  { href: '/page/terminos-y-condiciones-de-publicidad', label: 'Términos y condiciones de publicidad' },
+  { href: '/site-map', label: 'Mapa del sitio web' },
+];
+
+const mobileMenu = {
+  primary: [
+    {
+      href: 'https://mundopmmi.dragonforms.com/perspectivas?pk=MundoSubNav&r=@{encrypted_customer_id}',
+      label: 'Suscríbase',
+      target: '_blank',
+    },
+    ...topics.primary,
+    ...topics.expanded,
+  ],
+  secondary: [
+    ...topics.secondary,
+    { href: '/newsletters', label: 'Newsletters' },
+  ],
+};
+
+const desktopMenu = {
+  about: [
+    {
+      href: 'https://mundopmmi.dragonforms.com/perspectivas?pk=MundoSubNav&r=@{encrypted_customer_id}',
+      label: 'Suscríbase',
+      target: '_blank',
+    },
+    ...utilities,
+  ],
+  sections: [
+    ...topics.primary,
+    ...topics.expanded,
+    ...topics.secondary,
+  ],
+};
+
+
 module.exports = {
+  desktopMenu,
+  mobileMenu,
+  primary: {
+    items: [],
+  },
   secondary: {
-    items: [
-      {
-        href: 'https://mundopmmi.dragonforms.com/perspectivas?pk=MundoSubNav&r=@{encrypted_customer_id}',
-        label: 'Suscríbase',
-        target: '_blank',
-      },
-      { href: '/automatizacion', label: 'Automatización' },
-      { href: '/empaque', label: 'Empaque' },
-      { href: '/procesamiento', label: 'Procesamiento' },
-      { href: '/eventos', label: 'Eventos' },
-      { href: '/downloads', label: 'Recursos Digitales' },
-    ],
+    items: [...topics.primary],
   },
   tertiary: {
-    items: [
-      { href: '/search', label: 'Search', icon: 'search' },
-    ],
+    items: [],
   },
   footer: {
     items: [
@@ -27,36 +78,14 @@ module.exports = {
       { href: '/page/terminos-y-condiciones-de-publicidad', label: 'Términos y condiciones de publicidad' },
       { href: '/site-map', label: 'Mapa del sitio web' },
     ],
+    topics: topics.primary,
+    more: [
+      {
+        href: 'https://mundopmmi.dragonforms.com/perspectivas?pk=MundoSubNav&r=@{encrypted_customer_id}',
+        label: 'Suscríbase',
+        target: '_blank',
+      },
+      ...utilities,
+    ],
   },
-  menu: [
-    {
-      label: 'Temas',
-      items: [
-        { href: '/automatizacion', label: 'Automatización' },
-        { href: '/empaque', label: 'Empaque' },
-        { href: '/procesamiento', label: 'Procesamiento' },
-      ],
-    },
-    {
-      label: 'Recursos',
-      items: [
-        { href: '/eventos', label: 'Eventos' },
-        { href: '/leaders', label: 'Líderes' },
-        { href: '/videos', label: 'Vídeos' },
-        { href: '/downloads', label: 'Recursos Digitales' },
-      ],
-    },
-    {
-      label: 'Herramientas de usuario',
-      items: [
-        { href: 'https://mundopmmi.dragonforms.com/perspectivas?pk=MundoSubNav&r=@{encrypted_customer_id}', label: 'Suscríbase', target: '_blank' },
-        { href: 'https://www.pmmimediagroup.com/pw/mundo-pmmi', label: 'Anuncie', target: '_blank' },
-        { href: '/page/acerca-de-nosotros', label: 'Acerca de nosotros' },
-        { href: '/page/contacto', label: 'Contacto' },
-        { href: '/page/privacidad', label: 'Privacidad' },
-        { href: '/page/condiciones-de-uso', label: 'Condiciones de uso' },
-        { href: '/page/terminos-y-condiciones-de-publicidad', label: 'Términos y condiciones de publicidad' },
-      ],
-    },
-  ],
 };
