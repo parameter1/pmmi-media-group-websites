b7fb4beff4a5044322b42a195fb76811b536be74
Brian Miller <brian@parameter1.com>
2022-02-04 10:13:48 -0600

    add global 250 50 to pfw

--- sites/global.profoodworld.com/server/routes/website-section.js
+++ sites/global.profoodworld.com/server/routes/website-section.js
@@ -3,8 +3,15 @@ const queryFragment = require('@pmmi-media-group/package-theme-monorail/graphql/
 const leadersFragment = require('@pmmi-media-group/package-theme-monorail/graphql/fragments/leaders-section');
 const section = require('../templates/website-section');
 const leaders = require('../templates/website-section/leaders');
+const global250 = require('../templates/website-section/global-250');
+const global50 = require('../templates/website-section/global-50');
 
 module.exports = (app) => {
+  app.get('/:alias(global-250)', (_, res) => { res.marko(global250); });
+  app.get('/:alias(global-250/*)', (_, res) => { res.marko(global250); });
+  app.get('/:alias(global-50)', (_, res) => { res.marko(global50); });
+  app.get('/:alias(global-50/*)', (_, res) => { res.marko(global50); });
+
   app.get('/:alias(leaders)', withWebsiteSection({
     template: leaders,
     queryFragment: leadersFragment,
