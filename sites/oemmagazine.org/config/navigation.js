24966b8ca5fb3cc3affe2d4b82dd73a8ebbeec99
Shinsina <shinsina@att.net>
2022-02-07 10:01:27 -0600

    Add Supplier News to Resources Menu under Podcasts

--- sites/oemmagazine.org/config/navigation.js
+++ sites/oemmagazine.org/config/navigation.js
@@ -42,7 +42,7 @@ module.exports = {
         { href: '/leaders', label: 'OEM Partner Leaders' },
         { href: '/videos', label: 'Videos' },
         { href: '/podcasts', label: 'Podcasts' },
-        // { href: '/page/digital-editions', label: 'Digital Editions' },
+        { href: '/supplier-news', label: 'Supplier News' },
       ],
     },
     {
