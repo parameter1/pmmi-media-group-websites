2381fe223b6591f962034820695cfdbd8cd2e2e3
Brian Miller <brian@parameter1.com>
2022-02-10 06:33:15 -0600

    Register/add Leaders components

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -1,9 +1,11 @@
 
+import Leaders from '@parameter1/base-cms-marko-web-leaders/browser';
 import MonoRail from '@pmmi-media-group/package-theme-monorail/browser';
 
 const ImageSlider = () => import(/* webpackChunkName: "global-image-slider" */ './image-slider.vue');
 
 export default (Browser) => {
+  Leaders(Browser);
   MonoRail(Browser);
 
   Browser.register('GlobalImageSlider', ImageSlider);
