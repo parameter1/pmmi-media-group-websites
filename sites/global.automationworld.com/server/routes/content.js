3f2fb3d3b489d6622ee61001b3cba2fc8ead17bd
Brian Miller <brian@parameter1.com>
2022-02-11 14:31:35 -0600

    move content routes to global vs sites
    Can move back to sites if need be

--- sites/global.automationworld.com/server/routes/content.js
+++ sites/global.automationworld.com/server/routes/content.js
@@ -1,12 +1,15 @@
 const withContent = require('@pmmi-media-group/package-global/middleware/with-content');
+const companyQueryFragmentFn = require('@pmmi-media-group/package-global/graphql/fragment-factories/content-company');
+const contentQueryFragmentFn = require('@pmmi-media-group/package-global/graphql/fragment-factories/content-page');
 const queryFragment = require('@pmmi-media-group/package-theme-monorail/graphql/fragments/content-page');
 const contact = require('@pmmi-media-group/package-global/templates/content/contact');
-const company = require('../templates/content/company');
-const mediaGallery = require('../templates/content/media-gallery');
-const whitepaper = require('../templates/content/whitepaper');
-const content = require('../templates/content');
+const company = require('@pmmi-media-group/package-global/templates/content/company');
+const mediaGallery = require('@pmmi-media-group/package-global/templates/content/media-gallery');
+const whitepaper = require('@pmmi-media-group/package-global/templates/content/whitepaper');
+const content = require('@pmmi-media-group/package-global/templates/content');
 
 module.exports = (app) => {
+  const { site } = app.locals;
   app.get('/*?contact/:id(\\d{8})*', withContent({
     template: contact,
     queryFragment,
@@ -14,21 +17,18 @@ module.exports = (app) => {
 
   app.get('/*?company/:id(\\d{8})*', withContent({
     template: company,
-    queryFragment,
+    queryFragment: companyQueryFragmentFn(site.get('leaders.alias')),
   }));
-
   app.get('/*?media-gallery/:id(\\d{8})*', withContent({
     template: mediaGallery,
-    queryFragment,
+    queryFragment: contentQueryFragmentFn(site.get('leaders.alias')),
   }));
-
   app.get('/*?whitepaper/:id(\\d{8})*', withContent({
     template: whitepaper,
-    queryFragment,
+    queryFragment: contentQueryFragmentFn(site.get('leaders.alias')),
   }));
-
   app.get('/*?:id(\\d{8})*', withContent({
     template: content,
-    queryFragment,
+    queryFragment: contentQueryFragmentFn(site.get('leaders.alias')),
   }));
 };
