a81c411f460de9ca2912d29bd9d257da46a63e3d
Brian Miller <brian@parameter1.com>
2022-02-14 14:10:53 -0600

    adjust get x to not allow for negative left alightment.
    This will prevent widget from opening outside the page container on mobile

--- packages/leaders-program/src/components/list/positions/menu-position.js
+++ packages/leaders-program/src/components/list/positions/menu-position.js
@@ -15,9 +15,18 @@ class MenuPosition extends AbstractPosition {
   get x() {
     const { calculus: calcs } = this;
     const { pageXOffset } = window;
-    if (this.opensAbove || this.opensBelow) return pageXOffset + calcs.link('left') + calcs.link('halfW') - calcs.menu('halfW');
-    if (this.opensLeft) return pageXOffset + calcs.link('left') - calcs.arrow('w') - calcs.menu('w');
-    return pageXOffset + calcs.link('right') + calcs.arrow('w');
+    console.log(this.opensAbove, this.opensBelow, this.opensLeft);
+    if (this.opensAbove || this.opensBelow) {
+      return ((pageXOffset + calcs.link('left') + calcs.link('halfW') - calcs.menu('halfW')) > 0)
+        ? pageXOffset + calcs.link('left') + calcs.link('halfW') - calcs.menu('halfW')
+        : 0;
+    }
+    if (this.opensLeft) {
+      return ((pageXOffset + calcs.link('left') - calcs.arrow('w') - calcs.menu('w') - 15) > 0)
+        ? pageXOffset + calcs.link('left') - calcs.arrow('w') - calcs.menu('w') - 15
+        : 0;
+    }
+    return ((pageXOffset + calcs.link('right') + calcs.arrow('w')) > 0) ? pageXOffset + calcs.link('right') + calcs.arrow('w') : 0;
   }
 
   get y() {
