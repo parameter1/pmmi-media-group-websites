37c99d7f3903b65fe26ea4619a88b66c1584965c
Brian Miller <brian@parameter1.com>
2022-02-15 14:40:05 -0600

    update chevron icons

--- packages/leaders-program/src/components/icons/chevron-right.vue
+++ packages/leaders-program/src/components/icons/chevron-right.vue
@@ -7,7 +7,7 @@
   >
     <!-- eslint-disable-next-line -->
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-      <path d="M9 18l6-6-6-6"/>
+      <path fill-rule="evenodd" d="M7.5 8l-5 5L1 11.5 4.75 8 1 4.5 2.5 3l5 5z" />
     </svg>
   </icon-wrapper>
 </template>
