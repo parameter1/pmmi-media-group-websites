04215e9cae6a4d7dc33c31be119840c9b9ded2f4
Shinsina <shinsina@att.net>
2022-04-20 14:26:48 -0500

    First set of translation supporting keys.

--- packages/theme-monorail/browser/translations-vue.js
+++ packages/theme-monorail/browser/translations-vue.js
@@ -1,8 +1,24 @@
 export default {
   en: {
     visitSiteLabel: 'Visit Site',
+    // Email Signup Privacy Policy
+    emailProviding: 'By providing your email, you agree to our',
+    protectedBy: 'This site is protected by reCAPTCHA and the Google',
+    privacyPolicy: 'Privacy Policy',
+    and: 'and',
+    termsOfService: 'Terms of Service',
+    apply: 'apply.',
+    signUp: 'Sign Up',
   },
   es: {
     visitSiteLabel: 'Visite el sitio',
+    // Email Signup Privacy Policy
+    emailProviding: 'By providing your email, you agree to our',
+    protectedBy: 'This site is protected by reCAPTCHA y the Google',
+    privacyPolicy: 'Privacy Policy',
+    and: 'y',
+    termsOfService: 'Terms of Service',
+    apply: 'apply.',
+    signUp: '¡Sign Up!',
   },
 };
