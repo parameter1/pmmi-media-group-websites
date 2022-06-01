0139aa1e38cb6488ca150a3445f6b9fd4ab205c3
Shinsina <shinsina@att.net>
2022-06-01 09:33:13 -0500

    Add explicit routes for “root” rss feed.

--- packages/global/routes/redirects.js
+++ packages/global/routes/redirects.js
@@ -5,11 +5,11 @@ module.exports = (app) => {
   app.get('/:alias(feed)', (req, res) => {
     res.redirect(301, '/__rss/website-scheduled-content.xml?input=%7B"sectionAlias"%3A"home"%7D');
   });
+  app.get('/rss', (_, res) => {
+    res.redirect(301, '/__rss/all-published-content.xml');
+  });
   app.get('/rss/:sectionAlias([a-z0-9-/]+)', (req, res) => {
     const { params } = req;
-    if (params.sectionAlias) {
-      res.redirect(301, `/__rss/website-scheduled-content.xml?input=${encodeURIComponent(JSON.stringify(params))}`);
-    }
-    res.redirect(301, '/_rss/all-published-content.xml');
+    res.redirect(301, `/__rss/website-scheduled-content.xml?input=${encodeURIComponent(JSON.stringify(params))}`);
   });
 };
--- packages/shared/routes/rss.js
+++ packages/shared/routes/rss.js
@@ -1,9 +1,9 @@
 module.exports = (app) => {
+  app.get('/rss', (_, res) => {
+    res.redirect(301, '/__rss/all-published-content.xml');
+  });
   app.get('/rss/:sectionAlias([a-z0-9-/]+)', (req, res) => {
     const { params } = req;
-    if (params.sectionAlias) {
-      res.redirect(301, `/__rss/website-scheduled-content.xml?input=${encodeURIComponent(JSON.stringify(params))}`);
-    }
-    res.redirect(301, '/_rss/all-published-content.xml');
+    res.redirect(301, `/__rss/website-scheduled-content.xml?input=${encodeURIComponent(JSON.stringify(params))}`);
   });
 };
