e83c1e67a320c18a5f01b91f4c3800c244738c56
Brian Miller <brian@parameter1.com>
2022-07-06 14:07:01 -0500

    Set hcp navigation items, breakpoints & font-size

--- sites/global.healthcarepackaging.com/config/navigation.js
+++ sites/global.healthcarepackaging.com/config/navigation.js
@@ -7,22 +7,59 @@ const topics = [
   { href: '/logistics-distribution', label: 'Logistics & Cold Chain' },
 ];
 
-const resources = [];
-
-const utilities = [];
+const utilities = [
+  { href: 'https://www.pmmimediagroup.com/hcp/healthcare-packaging', label: 'Advertise', target: '_blank' },
+  { href: 'https://www.pmmimediagroup.com/hcp/reprints-and-permissions', label: 'Reprints' },
+  { href: '/page/hcp-about-us', label: 'About' },
+  { href: '/page/hcp-contact-us', label: 'Contact' },
+];
 
 const mobileMenu = {
-  primary: [...topics],
+  primary: [
+    ...topics,
+  ],
   secondary: [
     subscribe,
-    resources,
+    { href: '/page/hcp-event-calendar', label: 'Events' },
+    { href: '/downloads', label: 'Downloads' },
+    { href: '/page/hcp-newsletters', label: 'Newsletters' },
+    { href: '/page/magazine', label: 'Magazine' },
+    { href: '/leaders', label: 'Premier Suppliers' },
+    { href: '/videos', label: 'Videos' },
+    { href: '/podcasts', label: 'Podcasts' },
+    { href: 'https://www.pmmi.org/hall-of-fame', label: 'Hall of Fame', target: '_blank' },
+    { href: '/webinars', label: 'Webinars' },
+    // { href: '/page/digital-editions', label: 'Digital,
   ],
 };
 
 const desktopMenu = {
-  about: [...utilities],
-  sections: [...topics],
-  resources: [...resources],
+  leftCol: {
+    items: [
+      subscribe,
+      ...utilities,
+    ],
+  },
+  middleCol: {
+    items: [
+      ...topics,
+      { href: '/issues', label: 'Issues' },
+    ],
+  },
+  rightCol: {
+    items: [
+      { href: '/page/hcp-event-calendar', label: 'Events' },
+      { href: '/downloads', label: 'Downloads' },
+      { href: '/page/hcp-newsletters', label: 'Newsletters' },
+      { href: '/page/magazine', label: 'Magazine' },
+      { href: '/leaders', label: 'Premier Suppliers' },
+      { href: '/videos', label: 'Videos' },
+      { href: '/podcasts', label: 'Podcasts' },
+      { href: 'https://www.pmmi.org/hall-of-fame', label: 'Hall of Fame', target: '_blank' },
+      { href: '/webinars', label: 'Webinars' },
+      // { href: '/page/digital-editions', label: 'Digital
+    ],
+  },
 };
 
 module.exports = {
@@ -32,7 +69,12 @@ module.exports = {
     items: [],
   },
   secondary: {
-    items: [...topics],
+    items: [
+      subscribe,
+      ...topics,
+      { href: '/leaders', label: 'Suppliers' },
+
+    ],
   },
   tertiary: {
     items: [],
@@ -45,7 +87,14 @@ module.exports = {
       { href: 'https://www.pmmimediagroup.com/advertising-terms-conditions', label: 'Advertising Terms & Conditions', target: '_blank' },
       { href: '/site-map', label: 'Site Map' },
     ],
-    topics: [...topics],
-    more: [...utilities],
+    topics: [
+      ...topics,
+      { href: '/issues', label: 'Issues' },
+    ],
+    more: [
+      subscribe,
+      { href: 'https://www.pmmimediagroup.com/hcp/healthcare-packaging', label: 'Advertise', target: '_blank' },
+      { href: '/page/hcp-contact-us', label: 'Contact' },
+    ],
   },
 };
