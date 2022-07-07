cf600abd67d27778d4825f04a27557903d4965f8
Brandon Krigbaum <brandonbk@gmail.com>
2022-07-07 14:30:17 -0500

    Add native-x placements

--- sites/global.packworld.com/config/native-x.js
+++ sites/global.packworld.com/config/native-x.js
@@ -4,6 +4,9 @@ const config = new NativeXConfiguration('https://pmmi.native-x.parameter1.com');
 
 config
   .setAliasPlacements('default', [
+    { name: 'announcement', id: '62c70a74c89aa300012e143c' },
+    { name: 'audience', id: '62c70a1608de4000016348e4' },
+    { name: 'marketing', id: '5da778ce65ebb90001f3ced6' },
     { name: 'default', id: '5da778d7d5766a00013c459d' },
   ]);
 
