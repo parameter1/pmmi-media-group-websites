4cbf9db46e436c98886b63e6e123e6790fd90d1e
Brian Miller <brian@parameter1.com>
2022-07-25 09:06:52 -0500

    Add dynamic site-menu positioner vue component
    This will allow for the the setting of the site-menu top to be dynamic to account for adding/removing of the new announcement block & any future adjustments to the height of the site-header.

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -2,10 +2,12 @@ import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
 import MonoRailLeaders from '@pmmi-media-group/package-theme-monorail-leaders/browser';
 
 const ImageSlider = () => import(/* webpackChunkName: "global-image-slider" */ './image-slider.vue');
+const DynamicSiteMenuPositioner = () => import(/* webpackChunkName: "dynamic-site-menu-positioner" */ './dynamic-site-menu-positioner.vue');
 
 export default (Browser) => {
   MonoRail(Browser);
   MonoRailLeaders(Browser);
 
+  Browser.register('DynamicSiteMenuPositioner', DynamicSiteMenuPositioner);
   Browser.register('GlobalImageSlider', ImageSlider);
 };
