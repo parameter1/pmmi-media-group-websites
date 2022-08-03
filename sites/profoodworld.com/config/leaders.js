995c49b981032ff2849e5f6b953d21f270f9dcb6
Brandon Krigbaum <brandonbk@gmail.com>
2022-08-02 20:21:05 -0500

    Update leaders aliases to 2022

--- sites/profoodworld.com/config/leaders.js
+++ sites/profoodworld.com/config/leaders.js
@@ -1,7 +1,7 @@
 module.exports = {
   enabled: process.env.LEADERS_ENABLED || true,
   title: 'Leaders in Processing',
-  alias: process.env.LEADERS_ALIAS || 'leaders-2021',
+  alias: process.env.LEADERS_ALIAS || 'leaders-2022',
   header: {
     imgSrc: process.env.LEADERS_LOGO || 'https://img.profoodworld.com/files/base/pmmi/all/leaders/pfw-2022.svg?auto=format,compress&h=90',
     imgSrcset: process.env.LEADERS_LOGO || 'https://img.profoodworld.com/files/base/pmmi/all/leaders/pfw-2022.svg?auto=format,compress&h=90&dpr=2 2x',
