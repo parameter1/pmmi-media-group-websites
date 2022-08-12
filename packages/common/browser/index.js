f340fb19d326f3a68898c86ee72a77a2bbcf9e44
Josh Worden <josh@parameter1.com>
2022-08-12 10:23:54 -0500

    Remove shared, common packages

--- packages/common/browser/index.js
+++
@@ -1,5 +0,0 @@
-const ImageSlider = () => import(/* webpackChunkName: "common-image-slider" */ './image-slider.vue');
-
-export default (Browser) => {
-  Browser.register('CommonImageSlider', ImageSlider);
-};
