9beb64ec6c0bfe05f45a7c08fc1d2855288bca29
Josh Worden <josh@parameter1.com>
2022-08-12 10:23:54 -0500

    Remove shared, common packages

--- packages/shared/templates/user/utils/get-login-referrer.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req) => {
-  const referrerUrl = req.get('Referrer');
-  if (!referrerUrl) return undefined;
-  const referrer = new URL(referrerUrl);
-  if (req.hostname !== referrer.hostname || req.originalUrl === referrer.pathname) return undefined;
-  return referrer.pathname;
-};
