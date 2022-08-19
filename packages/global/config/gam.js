b6dc482319148ca7c351bc12dbcdb7dbcd688720
Brandon Krigbaum <brandonbk@gmail.com>
2022-08-18 20:53:14 -0500

    Move lazyload config to global

--- packages/global/config/gam.js
+++ packages/global/config/gam.js
@@ -6,6 +6,13 @@ module.exports = ({
 } = {}) => {
   const config = new GAMConfiguration(accountId, { basePath });
 
+  config.lazyLoad = {
+    enabled: true, // set to true to enable lazy loading
+    fetchMarginPercent: 100, // fetch ad when one viewport away
+    renderMarginPercent: 50, // render ad when half viewport away
+    mobileScaling: 2, // double these on mobile
+  };
+
   config
     .setTemplate('LEADERBOARD', {
       size: [
