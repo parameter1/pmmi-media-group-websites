125195edce56e09166d7313170d3fe5120780230
Brian Miller <brian@parameter1.com>
2022-08-26 11:06:18 -0500

    Move when and how newsletterState cookie setting on content
    Add the ability to pass setCookie prop to newsletterState to allow for this to be handled later in the route on content once a gating check is run.

--- packages/global/middleware/newsletter-state.js
+++ packages/global/middleware/newsletter-state.js
@@ -1,71 +1,29 @@
-const { asyncRoute } = require('@parameter1/base-cms-utils');
 const { get } = require('@parameter1/base-cms-object-path');
-const gql = require('graphql-tag');
-
-// Content is currently gated by reg fragment
-const query = gql`
-  query content($input: ContentQueryInput!) {
-    content(input: $input){
-      id
-      type
-      userRegistration {
-        isCurrentlyRequired
-      }
-    }
-  }
-`;
-
-/**
- * @param {object} req The Express request object.
- */
-async function getContent(req) {
-  const { apollo, params } = req;
-  const id = Number(params.id);
-  const variables = { input: { id } };
-  const { data } = await apollo.query({ query, variables });
-  const { content } = data;
-  return content;
-}
 
 const cookieName = 'enlPrompted';
 
-module.exports = () => asyncRoute(async (req, res, next) => {
-  let disableGate = false;
-  // check if there is a content id in the request
-  const contentRegex = new RegExp(/\d{8}/, 'i');
-  const isContent = contentRegex.test(req.params.id);
-  // Only setting this up this way for future use to override from a config elsewhere
-  const config = {
-    excludeGatedContent: true,
-    excludeContentTypes: [
-      'page',
-    ],
-  };
-  // If there is a content id see if it is a gated piece of content
-  if (isContent) {
-    if (config.excludeGatedContent
-      || (config.excludeContentTypes && config.excludeContentTypes.length !== 0)
-    ) {
-      const content = await getContent(req);
-      const gatedContent = get(content, 'userRegistration.isCurrentlyRequired') || false;
-      const isExcludedByType = config.excludeContentTypes.includes(content.type);
-      disableGate = gatedContent || isExcludedByType;
-    }
-  }
+module.exports = ({ setCookie = true } = {}) => (req, res, next) => {
   const hasCookie = Boolean(get(req, `cookies.${cookieName}`));
   const utmMedium = get(req, 'query.utm_medium');
   const olyEncId = get(req, 'query.oly_enc_id');
   const disabled = get(req, 'query.newsletterDisabled');
   const fromEmail = utmMedium === 'email' || olyEncId || false;
-  const initiallyExpanded = !disableGate && !(hasCookie || fromEmail || disabled);
+  const initiallyExpanded = (setCookie === true) && !(hasCookie || fromEmail || disabled);
+
+  // Expire in 14 days (2yr if already signed up)
+  const days = fromEmail ? 365 * 2 : 14;
+  const maxAge = days * 24 * 60 * 60 * 1000;
 
   if (initiallyExpanded) {
-    // Expire in 14 days (2yr if already signed up)
-    const days = fromEmail ? 365 * 2 : 14;
-    const maxAge = days * 24 * 60 * 60 * 1000;
     res.cookie(cookieName, true, { maxAge });
   }
 
-  res.locals.newsletterState = { hasCookie, fromEmail, initiallyExpanded };
+  res.locals.newsletterState = {
+    hasCookie,
+    fromEmail,
+    disabled,
+    initiallyExpanded,
+    cookie: { name: cookieName, maxAge },
+  };
   next();
-});
+};
--- packages/global/routes/content.js
+++ packages/global/routes/content.js
@@ -1,3 +1,4 @@
+const { get } = require('@parameter1/base-cms-object-path');
 const queryFragment = require('@parameter1/base-cms-marko-web-theme-monorail/graphql/fragments/content-page');
 const companyQueryFragmentFn = require('@pmmi-media-group/package-theme-monorail-leaders/graphql/fragment-factories/content-company');
 const contentQueryFragmentFn = require('@pmmi-media-group/package-theme-monorail-leaders/graphql/fragment-factories/content-page');
@@ -9,32 +10,55 @@ const company = require('../templates/content/company');
 const mediaGallery = require('../templates/content/media-gallery');
 const whitepaper = require('../templates/content/whitepaper');
 const webinar = require('../templates/content/webinar');
-const content = require('../templates/content');
+const contentTemplate = require('../templates/content');
+
+const formatResponse = ({ res, content }) => {
+  const {
+    initiallyExpanded,
+    hasCookie,
+    fromEmail,
+    disabled,
+    cookie,
+  } = res.locals.newsletterState;
+
+  if (get(content, 'userRegistration.isCurrentlyRequired') === true) {
+    res.locals.newsletterState.initiallyExpanded = false;
+  } else if (!initiallyExpanded && !hasCookie && !disabled && !fromEmail) {
+    res.cookie(cookie.name, true, { maxAge: cookie.maxAge });
+    res.locals.newsletterState.initiallyExpanded = true;
+  }
+};
 
 module.exports = (app) => {
   const { site } = app.locals;
-  app.get('/*?contact/:id(\\d{8})*', newsletterState(), withContent({
+  app.get('/*?contact/:id(\\d{8})*', newsletterState({ setCookie: false }), withContent({
     template: contact,
     queryFragment,
+    formatResponse,
   }));
-  app.get('/*?company/:id(\\d{8})*', newsletterState(), withContent({
+  app.get('/*?company/:id(\\d{8})*', newsletterState({ setCookie: false }), withContent({
     template: company,
     queryFragment: companyQueryFragmentFn(site.get('leaders.alias')),
+    formatResponse,
   }));
-  app.get('/*?media-gallery/:id(\\d{8})*', newsletterState(), withContent({
+  app.get('/*?media-gallery/:id(\\d{8})*', newsletterState({ setCookie: false }), withContent({
     template: mediaGallery,
     queryFragment: contentQueryFragmentFn(site.get('leaders.alias')),
+    formatResponse,
   }));
-  app.get('/*?whitepaper/:id(\\d{8})*', newsletterState(), withContent({
+  app.get('/*?whitepaper/:id(\\d{8})*', newsletterState({ setCookie: false }), withContent({
     template: whitepaper,
     queryFragment: contentQueryFragmentFn(site.get('leaders.alias')),
+    formatResponse,
   }));
-  app.get('/*?webinar/:id(\\d{8})*', newsletterState(), withContent({
+  app.get('/*?webinar/:id(\\d{8})*', newsletterState({ setCookie: false }), withContent({
     template: webinar,
     queryFragment,
+    formatResponse,
   }));
-  app.get('/*?:id(\\d{8})*', newsletterState(), withContent({
-    template: content,
+  app.get('/*?:id(\\d{8})*', newsletterState({ setCookie: false }), withContent({
+    template: contentTemplate,
     queryFragment: contentQueryFragmentFn(site.get('leaders.alias')),
+    formatResponse,
   }));
 };
