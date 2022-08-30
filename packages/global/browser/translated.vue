03d39fbc44fdab4cb408dd107fa80232fab04569
Shinsina <shinsina@att.net>
2022-08-30 11:37:39 -0500

    Test Vue Translations component.

---
+++ packages/global/browser/translated.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{ translate('firstNameLabel') }}
+  </div>
+</template>
+
+<script>
+import i18n from '../utils/i18n-vue';
+
+export default {
+  props: {
+    lang: {
+      type: String,
+      default: 'en',
+    },
+  },
+  methods: {
+    translate(key) {
+      return i18n(this.lang, key);
+    },
+  },
+};
+</script>
