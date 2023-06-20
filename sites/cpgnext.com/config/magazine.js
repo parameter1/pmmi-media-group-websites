d9a8b99a0445dfb45572b3c983d2c19b352dc543
Jessica Wade <jessica.wade@parameter1.com>
2023-06-20 09:26:28 -0500

    Allow to show PackWorld magazine to prevent error
    ‘length’ error is thrown if no magazine is present

--- sites/cpgnext.com/config/magazine.js
+++ sites/cpgnext.com/config/magazine.js
@@ -1,6 +1,6 @@
-// module.exports = {
-//   publicationIds: [
-//     '5d8a239af6d5f267ee9745d0',
-//   ],
-//   buttons: ['subscribe', 'renew', 'archives'],
-// };
+module.exports = {
+  publicationIds: [
+    '5d8a239af6d5f267ee9745d0',
+  ],
+  buttons: ['subscribe', 'renew', 'archives'],
+};
