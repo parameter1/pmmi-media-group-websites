7e10044ad693b44116957572bb5a4f6876ca32c7
Shinsina <shinsina@att.net>
2023-10-30 14:10:07 -0500

    Include site reading-progress-bar Vue component

---
+++ sites/cpgnext.com/browser/reading-progress-bar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="reading-progress-bar" class="reading-progress-bar" />
+</template>
+
+<script>
+export default {
+  created() {
+    window.addEventListener('scroll', this.setProgress);
+  },
+  methods: {
+    setProgress() {
+      const { body } = document;
+      const html = document.documentElement;
+      const height = Math.max(
+        body.scrollHeight,
+        body.offsetHeight,
+        html.clientHeight,
+        html.scrollHeight,
+        html.offsetHeight,
+      );
+      const progress = document.getElementById('reading-progress-bar');
+      const scrollFromTop = (html.scrollTop || body.scrollTop) + html.clientHeight;
+      const width = `${(scrollFromTop / height) * 100}%`;
+      progress.style.width = width;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
