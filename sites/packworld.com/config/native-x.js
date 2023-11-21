10ee8e726c4266eff18753433cb85d07e1bededb
Shinsina <shinsina@att.net>
2023-11-21 13:34:14 -0600

    + Native-X placement for super-category-labeling

--- sites/packworld.com/config/native-x.js
+++ sites/packworld.com/config/native-x.js
@@ -7,6 +7,7 @@ config
     { name: 'announcement', id: '62c70a74c89aa300012e143c' },
     { name: 'audience', id: '62c70a1608de4000016348e4' },
     { name: 'marketing', id: '5da778ce65ebb90001f3ced6' },
+    { name: 'super-category-labeling', id: '655d00bee9037c000183a628' },
     { name: 'default', id: '5da778d7d5766a00013c459d' },
   ]);
 
