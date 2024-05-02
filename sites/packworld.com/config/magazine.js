f01f96975cc73c228fbfab9f82b71a738a37256b
Shinsina <shinsina@att.net>
2024-05-02 14:21:47 -0500

    Swap publicationId to Contract Packaging Magazine

--- sites/packworld.com/config/magazine.js
+++ sites/packworld.com/config/magazine.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicationIds: [
-    '5d8a239af6d5f267ee9745d0',
+    '5e32e57784d0b1a4088b4733',
   ],
   buttons: ['subscribe', 'digital-edition', 'renewal', 'change-address', 'cancel', 'archives'],
 };
