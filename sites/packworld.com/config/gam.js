4e11a5c97fc753317017c30a5a5c34061ffae3f2
Shinsina <shinsina@att.net>
2024-05-21 11:31:30 -0500

    Utilize basePath option for PW

--- sites/packworld.com/config/gam.js
+++ sites/packworld.com/config/gam.js
@@ -1,13 +1,13 @@
 const configureGAM = require('@pmmi-media-group/package-global/config/gam');
 
-const config = configureGAM();
+const config = configureGAM({ basePath: 'pw' });
 
 config.setAliasAdUnits('default', [
-  { name: 'leaderboard', templateName: 'LEADERBOARD', path: 'pw/rotation' },
-  { name: 'rotation', templateName: 'ROTATION', path: 'pw/rotation' },
-  { name: 'inline-content', templateName: 'INLINE-CONTENT', path: 'pw/rotation' },
-  { name: 'inline-content-mobile', templateName: 'INLINE-CONTENT-MOBILE', path: 'pw/rotation' },
-  { name: 'inline-content-desktop', templateName: 'INLINE-CONTENT-DESKTOP', path: 'pw/rotation' },
+  { name: 'leaderboard', templateName: 'LEADERBOARD', path: 'rotation' },
+  { name: 'rotation', templateName: 'ROTATION', path: 'rotation' },
+  { name: 'inline-content', templateName: 'INLINE-CONTENT', path: 'rotation' },
+  { name: 'inline-content-mobile', templateName: 'INLINE-CONTENT-MOBILE', path: 'rotation' },
+  { name: 'inline-content-desktop', templateName: 'INLINE-CONTENT-DESKTOP', path: 'rotation' },
 ]);
 
 module.exports = config;
