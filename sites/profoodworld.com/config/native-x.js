c5e091a787cecc11d77d896d89a863a432c55a0b
Shinsina <shinsina@att.net>
2024-08-14 09:36:20 -0500

    Configure audience placements for sections PFW

--- sites/profoodworld.com/config/native-x.js
+++ sites/profoodworld.com/config/native-x.js
@@ -8,6 +8,24 @@ config
     { name: 'audience', id: '62c709c1c89aa300012dfc8e' },
     { name: 'marketing', id: '5da778b1d5766a00013c4563' },
     { name: 'default', id: '5da778bd65ebb90001f3ceab' },
+  ])
+  .setAliasPlacements('automation', [
+    { name: 'audience', id: '66bcbdb51a1dc2f012627998' },
+  ])
+  .setAliasPlacements('processing-equipment', [
+    { name: 'audience', id: '66bcbdc61a1dc2f01262799a' },
+  ])
+  .setAliasPlacements('facilities-design-infrastructure', [
+    { name: 'audience', id: '66bcbdd52aefeaff78937dc3' },
+  ])
+  .setAliasPlacements('food-safety', [
+    { name: 'audience', id: '66bcbde11a1dc2f01262799c' },
+  ])
+  .setAliasPlacements('sustainability', [
+    { name: 'audience', id: '66bcbdef1a1dc2f01262799e' },
+  ])
+  .setAliasPlacements('packaging-technology', [
+    { name: 'audience', id: '66bcbdfc1a1dc2f0126279a0' },
   ]);
 
 module.exports = config;
