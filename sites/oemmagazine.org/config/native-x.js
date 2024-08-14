ffae171968282e7fc26e5b35e6deb5410d9d203a
Shinsina <shinsina@att.net>
2024-08-14 09:10:34 -0500

    Configure audience placements for sections OEM

--- sites/oemmagazine.org/config/native-x.js
+++ sites/oemmagazine.org/config/native-x.js
@@ -8,6 +8,12 @@ config
     { name: 'audience', id: '62c70986c89aa300012df2f4' },
     { name: 'marketing', id: '5da7789b65ebb90001f3ce71' },
     { name: 'default', id: '5da778a4d5766a00013c4538' },
+  ])
+  .setAliasPlacements('engineering', [
+    { name: 'audience', id: '66bcb63e2aefeaff78937db9' },
+  ])
+  .setAliasPlacements('business', [
+    { name: 'audience', id: '66bcb6111a1dc2f01262798e' },
   ]);
 
 module.exports = config;
