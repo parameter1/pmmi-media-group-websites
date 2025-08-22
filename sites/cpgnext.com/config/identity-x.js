90498fcdf30e9244ba9c55faa8d8259892d37640
Brandon Krigbaum <brandonbk@gmail.com>
2025-08-21 19:41:23 -0500

    Delete cpgnext

--- sites/cpgnext.com/config/identity-x.js
+++
@@ -1,8 +0,0 @@
-const configureIdentityX = require('@pmmi-media-group/package-global/config/identity-x');
-
-module.exports = configureIdentityX({
-  appId: process.env.IDENTITYX_APP_ID || '649063f19a64332c8ec42eed',
-  gtmUserFields: {
-    primary_business: '64e4b0ab178ace3bf5003c1f',
-  },
-});
--- sites/cpgnext.com/config/search.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  contentTypes: ['Article', 'Blog', 'Company', 'Document', 'MediaGallery', 'News', 'Page', 'Podcast', 'Product', 'Video', 'Webinar'],
-  assignedToWebsiteSectionIds: [
-    86438,
-  ],
-  defaultSortField: 'PUBLISHED',
-  type: 'MATCH_PHRASE',
-  useMindful: true,
-};
