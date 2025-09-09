d3aca6467eafe1ac68244d9f61a7f15f5df7e666
Jessica Wade <jessica.wade@parameter1.com>
2025-09-09 13:10:59 -0500

    Create page details for podcasts page

---
+++ sites/mundopmmi.com/config/page-details.js
@@ -0,0 +1,17 @@
+module.exports = {
+  podcasts: {
+    name: 'Voces desde la línea: Pódcast de Empaque y Procesamiento',
+    description: 'Este pódcast de Mundo EXPO PACK explora y analiza las tecnologías, tendencias, estrategias e ideas que están revolucionando cómo procesamos, empacamos y llevamos los productos de consumo empacados al mercado. Buscamos que cada episodio de “Voces desde la Línea” sea una ventana al futuro de nuestra industria. Prepárese para estar siempre un paso adelante.',
+    primaryImage: {
+      src: 'https://img.healthcarepackaging.com/files/base/pmmi/all/image/static/Podcast_Logo.png',
+      alt: 'Voces desde la línea: Pódcast de Empaque y Procesamiento',
+    },
+    socialLinks: [
+      {
+        label: 'Buzzsprout',
+        provider: 'buzzsprout',
+        url: 'https://www.buzzsprout.com/2464494',
+      },
+    ],
+  },
+};
