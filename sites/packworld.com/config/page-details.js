56e797ed50ee219f1ea47fedfa56acfa239201a7
Jessica Wade <jessica.wade@parameter1.com>
2025-10-28 13:16:43 -0500

    Add images to page-details

--- sites/packworld.com/config/page-details.js
+++ sites/packworld.com/config/page-details.js
@@ -1,11 +1,44 @@
 module.exports = {
   'industries/beverage': {
     feedDescription: 'Packaging World’s beverage content focuses on beverage packaging and materials innovations, automation, machinery, and sustainability. Verticals within the scope of editorial coverage include: <a href="/industries/beverage/beer">beer</a>, <a href="/industries/beverage/coffee">coffee</a>, <a href="/industries/beverage/dairy">dairy</a>, <a href="/industries/beverage/drinks-nonalcoholic">drinks/non-alcoholic</a>, <a href="/industries/beverage/juice">juice</a>, <a href="/industries/beverage/spirits-liquors">spirits/liquors</a>, <a href="/industries/beverage/water">water</a>, and <a href="/industries/beverage/wine">wine</a>.',
+    primaryImage: {
+      src: 'https://img.packworld.com/files/base/pmmi/pw/beverage.jpg',
+      alt: 'Beverage',
+      imgOpt: {
+        crop: 'focalPoint',
+        fit: 'fillMax',
+        w: 160,
+        h: 160,
+        bg: 'ffffff',
+      },
+    },
   },
   'industries/food': {
     feedDescription: 'Packaging World’s food content focuses on food packaging and materials innovations, automation, machinery, and sustainability. Verticals within the scope of editorial coverage include: <a href="/industries/food/bakery-snack">bakery/snack</a>, <a href="/industries/food/cereals-breakfast-foods">cereals and breakfast foods</a>, <a href="/industries/food/dairy">dairy</a>, <a href="/industries/food/confection-candy">confection/candy</a>, <a href="/industries/food/fruits-vegetables">fruit/vegetables</a>, <a href="/industries/food/meat-poultry-seafood">meat/poultry/seafood</a>, <a href="/industries/food/petfood-pet-care">pet food</a>, <a href="/industries/food/prepared-foods">prepared foods</a>, and <a href="/industries/food/soups-condiments-sauces-spices-dips-dressings">soups/condiments/spices/dressings</a>.',
+    primaryImage: {
+      src: 'https://img.packworld.com/files/base/pmmi/pw/food.jpg',
+      alt: 'Beverage',
+      imgOpt: {
+        crop: 'focalPoint',
+        fit: 'fillMax',
+        w: 160,
+        h: 160,
+        bg: 'ffffff',
+      },
+    },
   },
   'industries/other-packaged-goods': {
     feedDescription: 'Packaging World’s other packaged goods content focuses on consumer goods packaging and materials innovations, automation, machinery, and sustainability. Verticals within the scope of editorial coverage include: <a href="/industries/other-packaged-goods/consumer-packaged-goods">consumer packaged goods</a>, <a href="/industries/other-packaged-goods/cosmetics-personal-care">cosmetics/personal care</a>, <a href="/industries/other-packaged-goods/detergents-cleaners">detergents/cleaners</a>, and <a href="/industries/other-packaged-goods/household-automotive">household/automotive items</a>.',
+    primaryImage: {
+      src: 'https://img.packworld.com/files/base/pmmi/pw/other-goods.jpg',
+      alt: 'Beverage',
+      imgOpt: {
+        crop: 'focalPoint',
+        fit: 'fillMax',
+        w: 160,
+        h: 160,
+        bg: 'ffffff',
+      },
+    },
   },
 };
