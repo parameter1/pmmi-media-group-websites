943fadabd651e99b153b0920c60a5a1bf4c8f80f
Jessica Wade <jessica.wade@parameter1.com>
2025-11-03 13:44:55 -0600

    Remove image options from page details

--- sites/packworld.com/config/page-details.js
+++ sites/packworld.com/config/page-details.js
@@ -4,38 +4,20 @@ module.exports = {
     primaryImage: {
       src: 'https://img.packworld.com/files/base/pmmi/pw/beverage.jpg',
       alt: 'Beverage',
-      imgOpt: {
-        fit: 'fill',
-        w: 250,
-        h: 167,
-        bg: 'ffffff',
-      },
     },
   },
   'industries/food': {
     feedDescription: 'Packaging World’s food content focuses on food packaging and materials innovations, automation, machinery, and sustainability. Verticals within the scope of editorial coverage include: <a href="/industries/food/bakery-snack">bakery/snack</a>, <a href="/industries/food/cereals-breakfast-foods">cereals and breakfast foods</a>, <a href="/industries/food/dairy">dairy</a>, <a href="/industries/food/confection-candy">confection/candy</a>, <a href="/industries/food/fruits-vegetables">fruit/vegetables</a>, <a href="/industries/food/meat-poultry-seafood">meat/poultry/seafood</a>, <a href="/industries/food/petfood-pet-care">pet food</a>, <a href="/industries/food/prepared-foods">prepared foods</a>, and <a href="/industries/food/soups-condiments-sauces-spices-dips-dressings">soups/condiments/spices/dressings</a>.',
     primaryImage: {
       src: 'https://img.packworld.com/files/base/pmmi/pw/food.jpg',
-      alt: 'Beverage',
-      imgOpt: {
-        fit: 'fill',
-        w: 250,
-        h: 167,
-        bg: 'ffffff',
-      },
+      alt: 'Food',
     },
   },
   'industries/other-packaged-goods': {
     feedDescription: 'Packaging World’s other packaged goods content focuses on consumer goods packaging and materials innovations, automation, machinery, and sustainability. Verticals within the scope of editorial coverage include: <a href="/industries/other-packaged-goods/consumer-packaged-goods">consumer packaged goods</a>, <a href="/industries/other-packaged-goods/cosmetics-personal-care">cosmetics/personal care</a>, <a href="/industries/other-packaged-goods/detergents-cleaners">detergents/cleaners</a>, and <a href="/industries/other-packaged-goods/household-automotive">household/automotive items</a>.',
     primaryImage: {
       src: 'https://img.packworld.com/files/base/pmmi/pw/other-goods.jpg',
-      alt: 'Beverage',
-      imgOpt: {
-        fit: 'fill',
-        w: 250,
-        h: 167,
-        bg: 'ffffff',
-      },
+      alt: 'Packaged Goods',
     },
   },
 };
