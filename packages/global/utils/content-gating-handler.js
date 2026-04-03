6903d149fff4047bc474b6454218f09f7278760c
Shinsina <shinsina@att.net>
2026-04-03 13:56:35 -0500

    Drop the query parameter bypass

--- packages/global/utils/content-gating-handler.js
+++ packages/global/utils/content-gating-handler.js
@@ -1,10 +1,3 @@
-const olyticsCookie = require('@mindful-web/marko-web-omeda/olytics/customer-cookie');
 const { get } = require('@mindful-web/object-path');
 
-module.exports = ({ content, req }) => {
-  const incomingEncId = olyticsCookie.clean(req.query.oly_enc_id);
-
-  if (incomingEncId) return false;
-
-  return get(content, 'userRegistration.isCurrentlyRequired', false) && !get(content, 'userRegistration.bypassGating', false);
-};
+module.exports = ({ content }) => get(content, 'userRegistration.isCurrentlyRequired', false) && !get(content, 'userRegistration.bypassGating', false);
